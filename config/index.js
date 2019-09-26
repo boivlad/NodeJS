65d549f80f5e2d8607cfbbbf6607619e99b35ba7
Vladyslav Boichenko <44983140+boivlad@users.noreply.github.com>
2019-09-26 18:47:10 +0300

    refactoring

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const app  = require('./app');
+const express = require('./express');
+const routes = require('./routs');
+
+module.exports = {
+	app,
+	express,
+	routes,
+};
--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/online-store');
+const mongoose = require('mongoose');
+require('./app/models/product');
+const config = require('./config');
 
 const app = express();
-app.use(bodyParser.json());
+config.express(app);
+config.routes(app);
 
-const Product = mongoose.model('Product', {
-	id: Number,
-	name: String,
-	price: mongoose.Schema.Types.Decimal128,
-});
-// const products = [
-// {
-// 	id: 1,
-// 	name: 'phone2',
-// 	price: 300,
-// },
-// {
-// 	id: 2,
-// 	name: 'tablet',
-// 	price: 600,
-// }
-// ];
+const { appPort, mongoUri } = config.app;
 
-app.get(
-	'/products', 
-	(req, res) => Product.find()
-	.exec()
-	.then(products => res.json(products)),
-	);
-
-app.post(
-	'/products', 
-	(req, res) => Product.create(req.body)
-	.then(createdProduct => res.json(createdProduct)),
+mongoose.connect(mongoUri)
+.then(() => app.listen(
+	appPort,
+	() => console.log(`Listening on port ${appPort}...`))
+.catch(err => console.error(`Error connecting to mongo: ${mongoUri} `, err))
 );
-
-app.put(
-	'/products/:id', 
-	(req, res) => Product.findOneAndUpdate({id: req.params.id}, req.body)
-	.exec()
-	.then(product => res.json(product)),
-);
-// app.delete('/products/:id', (req, res) => {
-// 	const product = products.find(p => p.id === +req.params.id);
-// 	const productIndex = products.indexOf(product);
-// 	products.splice(productIndex, 1);
-// 	res.json({success: true});
-// });
-
-app.listen(3000, () => console.log('Listening on port 3000'));
