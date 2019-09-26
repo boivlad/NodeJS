c97aec8ec3a8d7cf7aa76f4a9f3babfc368e13c1
Vladyslav Boichenko <44983140+boivlad@users.noreply.github.com>
2019-09-26 18:20:34 +0300

    Part 1

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/online-store');
 
 const app = express();
 app.use(bodyParser.json());
 
-const products = [
-{
-	id: 1,
-	name: 'phone2',
-	price: 300,
-},
-{
-	id: 2,
-	name: 'tablet',
-	price: 600,
-}
-];
-
-app.get('/products', (req, res) => res.json(products));
-
-app.post('/products', (req, res) => {
-	products.push(req.body);
-	res.json(req.body);
+const Product = mongoose.model('Product', {
+	id: Number,
+	name: String,
+	price: mongoose.Schema.Types.Decimal128,
 });
+// const products = [
+// {
+// 	id: 1,
+// 	name: 'phone2',
+// 	price: 300,
+// },
+// {
+// 	id: 2,
+// 	name: 'tablet',
+// 	price: 600,
+// }
+// ];
 
-app.put('/products/:id', (req, res) => {
-	const product = products.find(p => p.id === +req.params.id);
-	const productIndex = products.indexOf(product);
-	const newProduct = { ...product, ...req.body };
-	products[productIndex] = newProduct;
-	res.json(newProduct);
-});
-app.delete('/products/:id', (req, res) => {
-	const product = products.find(p => p.id === +req.params.id);
-	const productIndex = products.indexOf(product);
-	products.splice(productIndex, 1);
-	res.json({success: true});
-});
+app.get(
+	'/products', 
+	(req, res) => Product.find()
+	.exec()
+	.then(products => res.json(products)),
+	);
+
+app.post(
+	'/products', 
+	(req, res) => Product.create(req.body)
+	.then(createdProduct => res.json(createdProduct)),
+);
+
+app.put(
+	'/products/:id', 
+	(req, res) => Product.findOneAndUpdate({id: req.params.id}, req.body)
+	.exec()
+	.then(product => res.json(product)),
+);
+// app.delete('/products/:id', (req, res) => {
+// 	const product = products.find(p => p.id === +req.params.id);
+// 	const productIndex = products.indexOf(product);
+// 	products.splice(productIndex, 1);
+// 	res.json({success: true});
+// });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
